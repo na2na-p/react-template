f6e8dad4cfed062a1f830372bf099d97229c4de1
na2na-p <na2na@na2na.dev>
2023-02-05 23:32:34 +0900

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: 'setup-vitest.ts',
+    environment: "jsdom",
+    setupFiles: "setup-vitest.ts",
     /**
      * for in-source testing.
      * {@link https://vitest.dev/guide/in-source.html}
      */
-    includeSource: ['src/**/*.{ts,tsx}'],
+    includeSource: ["src/**/*.{ts,tsx}"],
   },
-})
+});
